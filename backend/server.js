94a796595599e6cbb86304838398a74e8706365b
Kevin Bustillos Acurio <kevinbustillosacurio@gmail.com>
2025-01-26 20:50:16 +0100

    Added backend API to be visible in React app

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
-const PORT = 6000;
+const PORT = 4000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/db';
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
 
 mongoose.connect(MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Failed to connect to MongoDB:', err));
 
-app.get('/', (req, res) => {
-  res.send('Backend is running');
+app.get('/health', (req, res) => {
+  res.json({ message: 'Backend is working!' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
